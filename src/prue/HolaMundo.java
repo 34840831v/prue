fcea008ca7e32ee86b3c58702cc4e1dd93e1d294
Javier <Javier@LAPTOP-8Q3H0GFR>
2017-06-13 17:54:54 +0200

    Mostramos hora actual

--- src/prue/HolaMundo.java
+++ src/prue/HolaMundo.java
@@ -1,10 +1,15 @@
 package prue;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-
+		Date ahora = new Date();
+	    SimpleDateFormat formateador = new SimpleDateFormat("hh:mm:ss");
+	    System.out.println(formateador.format(ahora));
 	}
 
 }
